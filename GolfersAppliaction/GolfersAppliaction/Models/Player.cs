234cced466ca24a26a073c1a6f0d080b9e283e32
Letjobana <suzanm@gerbertech.co.za>
2022-03-09 10:59:37 +0200

    Added a model folder and a class Player

---
+++ GolfersAppliaction/GolfersAppliaction/Models/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace GolfersAppliaction.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public double Price { get; set; }
+    }
+}
