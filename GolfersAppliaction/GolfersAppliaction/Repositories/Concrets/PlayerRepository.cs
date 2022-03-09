bc8443145282f689cd1483fa9f4058518de7c654
Letjobana <suzanm@gerbertech.co.za>
2022-03-09 11:35:49 +0200

    Updated PlayersRepository

---
+++ GolfersAppliaction/GolfersAppliaction/Repositories/Concrets/PlayerRepository.cs
@@ -0,0 +1,46 @@
+﻿using GolfersAppliaction.Models;
+using GolfersAppliaction.Persistance;
+using GolfersAppliaction.Repositories.Abstracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GolfersAppliaction.Repositories.Concrets
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public PlayerRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public Player AddPlayerScore(Player player)
+        {
+            if (player != null)
+            {
+                double price = player.Score * 10;
+                var playerScore = new Player
+                {
+                    Name = player.Name,
+                    Score = player.Score,
+                    Price = price
+                };
+                context.Players.Add(playerScore);
+                context.SaveChanges();
+            }
+
+            return player;
+
+        }
+
+        public IEnumerable<Player> GetAllPlayers()
+        {
+            return context.Players.ToList().OrderBy(x => x.Score);
+        }
+
+        public Player GetPlayerById(int Id)
+        {
+            return context.Players.Find(Id);
+        }
+    }
+}
