d65291ceda6939969f8324977947aaf13db50c82
Letjobana <suzanm@gerbertech.co.za>
2022-03-09 11:18:25 +0200

    Added repositories, IPlayerRepository and PlayeRepository

---
+++ GolfersAppliaction/GolfersAppliaction/Repositories/Abstracts/IPlayerRepository.cs
@@ -0,0 +1,12 @@
+﻿using GolfersAppliaction.Models;
+using System.Collections.Generic;
+
+namespace GolfersAppliaction.Repositories.Abstracts
+{
+    public interface IPlayerRepository
+    {
+        IEnumerable<Player> GetAllPlayers();
+        Player GetPlayerById(int Id);
+        Player AddPlayerScore(Player player);
+    }
+}
